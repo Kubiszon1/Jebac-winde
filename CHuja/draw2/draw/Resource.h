94eada95f503bcc912a79252a93819ab808e7494
Gawendz <105167719+Gawendz@users.noreply.github.com>
2022-06-24 22:13:20 +0200

    Add files via upload

---
+++ CHuja/draw2/draw/Resource.h
@@ -0,0 +1,71 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by draw.rc
+//
+
+#define IDS_APP_TITLE			103
+
+#define IDR_MAINFRAME			128
+#define IDD_DRAW_DIALOG	102
+#define IDD_ABOUTBOX			103
+#define IDM_ABOUT				104
+#define IDM_EXIT				105
+#define IDI_DRAW			107
+#define IDI_SMALL				108
+#define IDC_DRAW			109
+#define IDC_MYICON				2
+#ifndef IDC_STATIC
+#define IDC_STATIC				-1
+#endif
+// Values for other objects (eg. buttons)
+#define ID_BUTTON99				200
+// *** 1 pietro ***
+#define ID_BUTTON1				201
+#define ID_BUTTON2				202
+#define ID_BUTTON3				203
+#define ID_BUTTON4				204
+#define ID_BUTTON5				205
+#define ID_BUTTON27				229
+/// ***3 pietro ***
+#define ID_BUTTON6				206
+#define ID_BUTTON7				207
+#define ID_BUTTON8				208
+#define ID_BUTTON9				209
+#define ID_BUTTON10				212
+// *** 5 pietro ***
+#define ID_BUTTON11				213
+#define ID_BUTTON12				214
+#define ID_BUTTON13				215
+#define ID_BUTTON14				216
+#define ID_BUTTON15				217
+// *** 2 pietro ***
+#define ID_BUTTON16				218
+#define ID_BUTTON17				219
+#define ID_BUTTON18				220
+#define ID_BUTTON19				221
+#define ID_BUTTON20		        222
+#define ID_BUTTON28		        230
+// *** 4 pietro ***
+#define ID_BUTTON21				223
+#define ID_BUTTON22				224
+#define ID_BUTTON23				225
+#define ID_BUTTON24				226
+#define ID_BUTTON25				227
+#define ID_BUTTON26				228
+
+
+#define ID_RBUTTON1				210
+#define ID_RBUTTON2				211
+
+// Next default values for new objects
+//
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+
+#define _APS_NO_MFC					130
+#define _APS_NEXT_RESOURCE_VALUE	129
+#define _APS_NEXT_COMMAND_VALUE		32771
+#define _APS_NEXT_CONTROL_VALUE		1000
+#define _APS_NEXT_SYMED_VALUE		110
+#endif
+#endif
